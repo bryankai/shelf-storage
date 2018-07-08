0c1538c6ca415a10a3e17b4697d1440f5d7b6b4f
bryankai <brykai123@gmail.com>
2018-07-07 17:11:10 -0700

    Book form adds inputs to local state

--- src/components/BookSpaceModal.js
+++ src/components/BookSpaceModal.js
@@ -2,10 +2,32 @@ import React, { Component }  from 'react';
 import {Modal, Button, Input, Row} from 'react-materialize'
 
 class BookSpaceModal extends Component {
+  state = {
+    startDate: '',
+    endDate: '',
+    totalPrice: ''
+  };
+
+  handleSubmit = event => {
+    event.preventDefault()
+    // this.props.userLogin(this.state, this.props.history);
+    this.setState(this.state);
+    console.log(this.state)
+  }
+
+
+  // Need to add this in...
+  handlePriceCalc = (price) => {
+    if(this.state.startDate && this.state.endDate) {
+      const duration = this.state.endDate - this.state.startDate
+      const totalPrice = duration*price
+      this.setState({totalPrice})
+    }
+  }
 
   render() {
     const {name,
-      // price
+      price
     }  = this.props.space
     const modalStyle = {
       minHeight: '600px'
@@ -16,13 +38,20 @@ class BookSpaceModal extends Component {
         header={name}
         trigger={<Button>MODAL</Button>}>
         <Row>
-          <form className='book-form' id='book-form' onSubmit={event => console.log(event)}>
-            <Input name='on' type='date' onChange={function(e, value) {}} label="Start Date"/>
-            <Input name='on' type='date' onChange={function(e, value) {}} label="End Date"/>
-            {/* <Input type="password" label="password" s={12} />
-            <Input type="email" label="Email" s={12} /> */}
+          <form className='book-form' id='book-form' onSubmit={event => this.handleSubmit(event)}>
+            <Input name='on' type='date' label="Start Date"   id="startDate"
+              // need to implement price calc
+              onChange={event => this.setState({startDate: event.target.value})}
+            />
+            <Input name='on' type='date' label="End Date" id="endDate"
+              onChange={event => this.setState({endDate: event.target.value})}
+            />
+            <Input placeholder="Placeholder" label="First Name" />
+            <Button waves='light' className='book-space-button' type="submit" form="book-form" value="Book">Book</Button>
           </form>
-          <Button waves='light' className='book-space-button' type="submit" form="book-form" value="Book">Book</Button>
+        </Row>
+        <Row>
+          <p>{this.state.totalPrice?this.state.totalPrice:null}</p>
         </Row>
       </Modal>
     )
