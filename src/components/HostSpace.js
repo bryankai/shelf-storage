59bc074bcf8b4146318f7474437dc3a53851aa2a
bryankai <brykai123@gmail.com>
2018-07-18 23:02:43 -0700

    Removed duplicate Description on HostSpace

--- src/components/HostSpace.js
+++ src/components/HostSpace.js
@@ -13,15 +13,8 @@ class HostSpace extends Component {
 
     } = this.props.hostSpace
 
-    // Issue with this link directing to /host/host/spaces... or /spaces, but not host/spaces...
     const spaceOrdersLink = `/spaces/${id}/reservations`
     const spacePageLink = `/spaces/${id}`
-
-    const truncateString = (text, length=60) => {
-      const maxLength = length;
-      const trimmedString = text.substring(0, maxLength);
-      return text.length<=maxLength ? text : `${trimmedString}...`
-    }
 
     return (
         <Col s={12} m={12} l={12} key={id}>
@@ -32,7 +25,6 @@ class HostSpace extends Component {
             <p>{city},{state} {zip}</p>
             <h6>{size} sqft</h6>
             <h6>{description}</h6>
-            <h6>{truncateString(description,250)}</h6>
             <h6><Link to={spacePageLink} >Public Preview</Link></h6>
           </Card>
         </Col>
