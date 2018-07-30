454ac9297e141f2ddc2464e83d751aa2a7cd63c4
bryankai <brykai123@gmail.com>
2018-07-30 09:31:19 -0700

    Cleaned up index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import store from './store'
-// import { request, AuthenticationService } from './helper/helper'
 import './styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
